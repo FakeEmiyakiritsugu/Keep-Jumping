98c1e1a20f4f4e2ec8a4995cbbc212e2193b7388
FakeEmiyakiritsugu <1245622372@qq.com>
2025-12-26 20:55:35 +0800

    新增thirdpersoncontroller脚本，二段跳逻辑

---
+++ Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/NewPlayer/newThirdPersonController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector;
+using Invector.vCharacterController;
+
+public class newThirdPersonConller : vThirdPersonController
+{
+
+
+
+    #region
+    [Header("new Controller")]
+    [Tooltip("max air jump times")]
+    public int maxairjumptimes = 1;//最大空中跳跃次数
+
+
+
+    internal int currentairjumptimes = 0;//当前跳跃次数
+
+
+    #endregion
+
+    protected override void CheckGround()
+    {
+        CheckGroundDistance();
+        ControlMaterialPhysics();
+
+        if (groundDistance <= groundMinDistance)
+        {
+            isGrounded = true;
+
+            //在空中与地表发生变化
+            IsGroundedChanged();
+
+            if (!isJumping && groundDistance > 0.05f)
+                _rigidbody.AddForce(transform.up * (extraGravity * 2 * Time.deltaTime), ForceMode.VelocityChange);
+
+            heightReached = transform.position.y;
+        }
+        else
+        {
+            if (groundDistance >= groundMaxDistance)
+            {
+                // set IsGrounded to false 
+                isGrounded = false;
+                // check vertical velocity
+                verticalVelocity = _rigidbody.velocity.y;
+                // apply extra gravity when falling
+                if (!isJumping)
+                {
+                    _rigidbody.AddForce(transform.up * extraGravity * Time.deltaTime, ForceMode.VelocityChange);
+                }
+            }
+            else if (!isJumping)
+            {
+                _rigidbody.AddForce(transform.up * (extraGravity * 2 * Time.deltaTime), ForceMode.VelocityChange);
+            }
+        }
+    }
+    protected virtual void IsGroundedChanged()//空中与地面的状态发生变化
+    {
+        if (isGrounded)//从空中到地面你
+        {
+            currentairjumptimes = 0;
+        }
+    }
+}
--- Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/NewPlayer/newThirdpersoninput.cs
+++ Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/NewPlayer/newThirdpersoninput.cs
@@ -8,9 +8,31 @@ public class newThirdpersoninput : vThirdPersonInput
 {
     #region
     [Header("new Controller Input")]
+    //[Tooltip("Lerp speed between Camera States")]
     public KeyCode dashinput = KeyCode.LeftShift;//冲锋按键
 
+
+
+    [HideInInspector] public newThirdPersonConller dd;
     #endregion
+
+
+
+
+
+    //重写cc的初始化逻辑
+    protected override void InitilizeController()
+    {
+        dd = GetComponent<newThirdPersonConller>();
+        cc = dd;
+
+        if (dd != null)
+            dd.Init();
+
+
+    }
+
+
     protected override void InputHandle()
     {
         MoveInput();
@@ -20,6 +42,26 @@ public class newThirdpersoninput : vThirdPersonInput
         JumpInput();
     }
 
+    //跳跃条件
+    protected override bool JumpConditions()
+    {
+        bool jumpflag = false;
+        if (cc.isGrounded && dd.GroundAngle() < dd.slopeLimit && !dd.stopMove && !dd.isJumping)//一段跳
+        {
+            jumpflag = true;
+        }
+        else if (!dd.isGrounded && dd.currentairjumptimes < dd.maxairjumptimes)//允许空中条约
+        {
+            jumpflag = true;
+            dd.currentairjumptimes++;
+        }
+        else
+        {
+            jumpflag = false;
+        }
+        return jumpflag;
+    }
+    //冲刺输入检测
     protected virtual void DashInput()
     {
 
